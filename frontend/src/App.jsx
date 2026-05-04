40e2eed9ee2c1448086ee8975b6311883d6c59c3
AGTWellPlayed <aceeramosmedinaceli+github@gmail.com>
2026-05-04 16:28:32 +0800

    start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import fbIcon from './assets/facebook_icon.svg';
+
+
+function App() {
+    return (
+      <div>
+        <div className="header-text" style={{ marginBottom: '30px' }}>
+            <h1>Digital Inventory</h1>
+        </div>
+
+          <footer className="footer-layout">
+          <div className="footer-side-panel"></div>
+          <div className="read-the-docs">
+            For questions and comments, contact us at:{' '}
+            <a href="https://www.facebook.com/mis.office.earist" target="_blank" rel="noreferrer">
+              <img src={fbIcon} className="footer-icons" alt="Facebook" />
+            </a>
+          </div>
+        </footer>
+
+      </div>
+    );
+} 
+
+export default App;
